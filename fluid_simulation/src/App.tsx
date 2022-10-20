2c0cd2fe419f9597e9138cd6865349c89c4bfdee
laycookie <80195126+laycookie@users.noreply.github.com>
2022-10-19 19:23:12 -0700

    temp

--- fluid_simulation/src/App.tsx
+++ fluid_simulation/src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect, useState } from "react";
+import Box2DFactory from "box2d-wasm";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    Box2DFactory().then((box2D: typeof Box2D & EmscriptenModule) => {
+      console.log(box2D);
+    });
+  }, []);
+
   return (
     <>
       <h1>test</h1>
       <h2>test test</h2>
+      <canvas width="800" height="600"></canvas>
     </>
   );
 }
